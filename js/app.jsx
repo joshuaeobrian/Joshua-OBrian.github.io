d271a6789f52423557f3657dc35fdcbe455d034a
Josh O'Brian <joshuaeobrian@gmail.com>
2018-02-07 17:31:47 -0500

    Added react files

---
+++ js/app.jsx
@@ -0,0 +1,15 @@
+
+
+let About = React.createClass({
+    render: function () {
+        return (
+            <h2>About:</h2>
+        );
+    }
+});
+
+
+
+
+    // console.log("HEllo");
+    // ReactDOM.render(<About/>, document.getElementById("container"));
