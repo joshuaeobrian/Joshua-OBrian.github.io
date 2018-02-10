63bcc49d74917096475598566fa471e97730091c
Josh O'Brian <joshuaeobrian@gmail.com>
2018-02-10 17:34:04 -0500

    added some scroll to logic

--- js/app.jsx
+++ js/app.jsx
@@ -1,14 +1,21 @@
 // // import {Experiences} from "./Experiences";
 // // import {About} from "./About";
-
+let positions = ["about","experience","education","projects"];
+let current;
+function scrollTo(url) {
+    //Todo: if current is about and url is some flag then go to 0 or if url is some flag then go to 0
+    current = url.substring(url.indexOf("#")+1);
+    console.log(current);
+    $('html, body').animate({
+        scrollTop: $('#'+current).offset().top
+    }, 1000);
+    return false;
+}
 
 $(document).ready(function () {
     $("a.scrollTo").on('click',function(e) {
         var url = e.target.href;
-        var hash = url.substring(url.indexOf("#")+1);
-        $('html, body').animate({
-            scrollTop: $('#'+hash).offset().top
-        }, 1000);
+        scrollTo(url);
         return false;
     });
     $(".contact #close").on('click', function (e) {
@@ -19,5 +26,24 @@ $(document).ready(function () {
     });
     $(".contact button").on("click",function (e) {
         e.preventDefault();
+        //Todo: make api call to back end
+
+        $(".contact").hide();
+    });
+    //Todo: make section smarter
+    $("#go-up").on("click",function () {
+        //Todo: find current position
+        let position = positions[positions.indexOf(current)-1];
+        console.log(position);
+        //Todo: find the next one up
+        scrollTo(position);
+        return false;
+    });
+    $("#go-down").on("click",function () {
+        //Todo: find current position
+        let position = positions[positions.indexOf(current)+1];
+        //Todo: find the next one down
+        scrollTo(position);
+        return false;
     });
 });
